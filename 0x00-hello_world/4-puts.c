098fc5f3b4452da40a3ed4f65f42469764083254
Citizenlucid <ezimajsph@gmail.com>
2022-09-12 16:52:41 -0700

    preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main - print the string in the put function
 *
@@ -9,7 +8,7 @@
 */
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
+c       puts("\"Programming is like building a multilingual puzzle");
        return (0);
 }
 
